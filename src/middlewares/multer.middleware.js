409bc3b0609b0e0086c9d6ba7be2852d2c38d985
Brassalsa <sauravchauhan2k@gmail.com>
2023-12-29 19:31:30 +0530

    added middlewares

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/img");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname + uuidv4());
+  },
+});
+
+export const upload = multer({
+  storage,
+});
