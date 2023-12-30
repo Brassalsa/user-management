0afe6fd26526f0974b6992ee172139d92b864b42
Brassalsa <sauravchauhan2k@gmail.com>
2023-12-30 21:17:07 +0530

    feat(validation): add email validation, feat(asycnHandler): add error cb, refactor(ApiError): add statusCode in response

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -2,8 +2,14 @@ const asyncHandler = (fn) => async (req, res, next) => {
   try {
     await fn(req, res, next);
   } catch (err) {
-    res.status(err.code || 500).json({
+    if (req.errCb) {
+      req.errCb();
+    }
+
+    res.status(+err.statusCode || 500).json({
       success: false,
+      code: err.code,
+      statusCode: +err.statusCode || 500,
       message: err.message,
     });
   }
