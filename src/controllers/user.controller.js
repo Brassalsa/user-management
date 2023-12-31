1f880df188c894c2d66dd989810aa669f5da61bb
Brassalsa <sauravchauhan2k@gmail.com>
2023-12-31 15:58:50 +0530

    feat: add update user account

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,14 +9,19 @@ import { isEmailFormatCorrect } from "../utils/validation.js";
 // generate access token
 const generateAccessTokens = async (userId) => {
   try {
-    const user = await User.findById(userId);
-    const accessToken = user.generateAccessToken();
+    // get a user
+    const user = await User.findById(userId).select("-password");
+    // generate token
+    const token = user.generateAccessToken();
 
-    return accessToken;
+    return {
+      user,
+      token,
+    };
   } catch (error) {
     throw new ApiError(
       500,
-      "Something went wrong while generating referesh and access token"
+      "Something went wrong while generating access token"
     );
   }
 };
@@ -30,8 +35,11 @@ const registerUser = asyncHandler(async (req, res) => {
 
   // delete file if error occured
   req.errCb = () => {
-    if (avatarUrl) {
+    if (!avatarUrl) return;
+    try {
       fs.unlinkSync(avatarUrl);
+    } catch (err) {
+      console.log("Failed to delete previous profile image ", err);
     }
   };
 
@@ -47,7 +55,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   // check at least email or phone no. exist
   if (!email && !phone) {
-    throw new ApiError(400, "Please provide at least email or phone number.");
+    throw new ApiError(400, "Email or Phone number is required");
   }
 
   // check if user exists wtih same phone or email
@@ -80,4 +88,108 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "User registered Successfully"));
 });
 
-export { generateAccessTokens, registerUser };
+// login
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, phone, password } = req.body;
+
+  // check for email or phone
+  if (!email && !phone) {
+    throw new ApiError(400, "Email or Phone number is required");
+  }
+
+  // check for password
+  if (!password) {
+    throw new ApiError(400, "Password is required");
+  }
+
+  // get user details
+  const user = await User.findOne({
+    $or: [{ email }, { phone }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist");
+  }
+
+  // check password
+  const isPasswordCorrect = await user.isPasswordCorrect(password);
+  if (!isPasswordCorrect) {
+    throw new ApiError(401, "Wrong credentials");
+  }
+
+  // generate access token
+  const { user: loggedUser, token } = await generateAccessTokens(user._id);
+
+  res.status(200).json(
+    new ApiResponse(200, {
+      user: loggedUser,
+      token,
+    })
+  );
+});
+
+// update account details
+const updateAccountDetails = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { name, email, phone, avatar } = req.body;
+  // profile img path
+  const avatarUrl = req.files?.avatar?.[0].path;
+  const user = await User.findById(_id);
+  const prevAvatar = user.avatar;
+  let updateParams = {};
+
+  // check for non-empty fields
+  if (name && name.trim()) {
+    updateParams = { ...updateParams, name };
+  }
+  if (email && email.trim()) {
+    updateParams = { ...updateParams, email };
+  }
+  if (phone && phone.trim()) {
+    updateParams = { ...updateParams, phone };
+  }
+  if (avatarUrl) {
+    updateParams = { ...updateParams, avatar: avatarUrl };
+  }
+
+  // check if email already exists
+  if (user.email && email) {
+    throw new ApiError(400, "Email cannot be changed");
+  }
+
+  // check if phone number exists
+  if (user.phone && phone) {
+    throw new ApiError(400, "Phone number cannot be changed");
+  }
+
+  // update the user
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      $set: updateParams,
+    },
+    { new: true }
+  ).select("-password");
+
+  // delete previous profile image
+  if (prevAvatar) {
+    try {
+      fs.unlinkSync(prevAvatar);
+    } catch (err) {
+      console.log("Failed to delete previous profile image ", err);
+    }
+  }
+
+  res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { user: updatedUser },
+        "Account updated successfully"
+      )
+    );
+});
+
+// exports
+export { generateAccessTokens, registerUser, loginUser, updateAccountDetails };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  registerUser,
+  updateAccountDetails,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
+// register
 userRouter.post(
   "/register",
   upload.fields([
@@ -13,6 +19,22 @@ userRouter.post(
     },
   ]),
   registerUser
+);
+
+// login
+userRouter.post("/login", loginUser);
+
+// update
+userRouter.put(
+  "/account",
+  verifyJWT,
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+  ]),
+  updateAccountDetails
 );
 
 export default userRouter;
